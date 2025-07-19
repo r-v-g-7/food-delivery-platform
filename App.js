5ef95f7dc84bcd5ab9cc110777d1927baf7d84a6
r-v-g-7 <r.vinayak.garg77@gmail.com>
2025-07-20 01:19:38 +0530

    Reality of react and npm

---
+++ App.js
@@ -0,0 +1,10 @@
+const parent = React.createElement("div", { id: "parent" }, 
+    [React.createElement("div", { id: "child" }, 
+    [React.createElement("h1", {}, "Hello I'm h1"), 
+    React.createElement("h2", {}, "Hello I'm h2")]), 
+    React.createElement("div", { id: "child2" }, 
+    [React.createElement("h1", {}, "Hello I'm h1"), 
+    React.createElement("h2", {}, "Hello I'm h2")])])
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(parent)
