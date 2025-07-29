4256d44912273cd6b93a7c6f21ebda1a5b4eccfe
r-v-g-7 <r.vinayak.garg77@gmail.com>
2025-07-29 23:52:48 +0530

    Adding some testing routes through react-router-dom pakage

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import ReactDOM from "react-dom/client";
 import { Header } from "./components/Header";
 import { Body } from "./components/Body";
+import { createBrowserRouter } from "react-router-dom";
+import { About } from "./components/About";
+
 
 const App = () => {
   return (
@@ -10,6 +13,26 @@ const App = () => {
     </div>
   );
 };
+
+
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />
+  }, 
+  // {
+  //   path: "/about",
+  //   element: <About />
+  // }
+  // {
+  //   path: "/contactUs",
+  //   element: <ContactUs />
+  // }, 
+  // {
+  //   path: "/cart",
+  //   element: <Cart />
+  // }
+])
 
 const container = document.getElementById("root");
 const root = ReactDOM.createRoot(container);
---
+++ src/components/About.js
@@ -0,0 +1,5 @@
+export const About = () => {
+    return (<div className="about-container">
+        <h1>Welcome to about page</h1>
+    </div>)
+}
