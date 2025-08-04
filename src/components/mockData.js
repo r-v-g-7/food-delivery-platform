3e133ecc2683de525991eaeec41d66f5728df9a8
r-v-g-7 <r.vinayak.garg77@gmail.com>
2025-08-05 01:03:58 +0530

    Just changed the logo of the app

--- src/components/mockData.js
+++ src/components/mockData.js
@@ -1518,10 +1518,10 @@ export const resArr = [
   }
 ]
 
-export const LOGO_URL = "https://img.freepik.com/free-vector/food-shopping-logo-template-design_460848-10299.jpg"
+export const LOGO_URL = "https://www.pngmart.com/files/22/Grand-Theft-Auto-San-Andreas-Logo-PNG-HD.png"
 
 export const CLOUDINARY_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/"
 
-export const SWIGGY_API = "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.4652224&lng=77.5116898&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+export const SWIGGY_API = "https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.9684308&lng=72.8102053&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
 
 export const MENU_API = "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=27.900019863776738&lng=78.04083265364169&restaurantId="
